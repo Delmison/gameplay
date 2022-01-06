8b5e6c3601c45a227a176e5543ad064e2007484b
Delmison Seidel <55005824+Delmison@users.noreply.github.com>
2022-01-06 17:52:10 -0300

    criando lista

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,29 @@
 import React,{useState} from "react";
-import {View} from 'react-native';
+import {View, FlatList, Text} from 'react-native';
 import { ButtonAdd } from "../../components/ButtonAdd";
 import { CategorySelect } from "../../components/CategorySelect";
 import { Profile } from "../../components/Profile";
+import { ListHeader} from "../../components/ListHeader";
 import { styles } from "./styles";
 
 export function Home(){
     const [ category, setCategory]= useState('')
+
+    const appointments =[
+        {
+            id:'1',
+            guild:{
+                id:'1',
+                name: 'Lendários',
+                icon: null,
+                owner: true
+            },
+            categor:'1',
+            date:'06/01 as 18:00h',
+            description:"É hoje que vemos chegar ao challenger sem perder uma partida md10"
+        }
+    ]
+
     function handleCategorySelect(categoryId: string){
         categoryId === category ? setCategory('') : setCategory(categoryId);
     }
@@ -23,8 +40,18 @@ export function Home(){
                 setCategory={handleCategorySelect}
             />
             <View style={styles.content}>
-                
-
+                <ListHeader 
+                    title="Partidas agendadas"
+                    subtitle="Total 6"
+                />
+            <FlatList 
+            data={appointments}
+            keyExtractor={item => item.id}
+            renderItem={({item})=>(
+                <Text>{item.guild.name}</Text>
+            )}
+            
+            />
             </View>
         </View>
         </View>
